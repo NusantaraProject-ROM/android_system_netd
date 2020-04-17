244792b64885feb464cd641e93183573a3a13972
Genkzsz11 <Genkzsz11@gmail.com>
2020-04-17 12:37:07 +0700

    Revert "system/netd: Squash of app fw restriction commits"
    This reverts commit 3728ad515e79de853d10945ae8eb80968574fbb2.

--- server/BandwidthController.h
+++ server/BandwidthController.h
@@ -57,11 +57,6 @@ public:
     int addNiceApps(const std::vector<std::string>& appStrUid);
     int removeNiceApps(const std::vector<std::string>& appStrUid);
 
-    int addRestrictAppsOnInterface(const std::string& usecase, const std::string& iface,
-                                   const std::vector<std::string>& appStrUid);
-    int removeRestrictAppsOnInterface(const std::string& usecase, const std::string& iface,
-                                      const std::vector<std::string>& appStrUid);
-
     int setGlobalAlert(int64_t bytes);
     int removeGlobalAlert();
     int setGlobalAlertInForwardChain();
@@ -100,12 +95,6 @@ public:
 #endif
 
     std::string makeDataSaverCommand(IptablesTarget target, bool enable);
-
-    int manipulateRestrictAppsInOut(const std::string& usecase, const std::string& iface,
-                                    const std::vector<std::string>& appStrUids, IptOp appOp);
-
-    int manipulateRestrictApps(const std::vector<std::string>& appStrUids, const std::string& chain,
-                               std::vector<int /*appUid*/>& restrictAppUids, IptOp appOp);
 
     int manipulateSpecialApps(const std::vector<std::string>& appStrUids, const std::string& chain,
                               IptJumpOp jumpHandling, IptOp appOp);
@@ -151,8 +140,6 @@ public:
 
     std::map<std::string, QuotaInfo> mQuotaIfaces;
     std::set<std::string> mSharedQuotaIfaces;
-
-    std::map<std::string /* interface name*/, std::vector<int /*appUid*/>> mRestrictAppsOnInterface;
 };
 
 #endif
